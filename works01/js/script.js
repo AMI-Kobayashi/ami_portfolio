63afc7f48015e75a1b46088cdfc3558a5d743f8e
AMI-Kobayashi <140169096+AMI-Kobayashi@users.noreply.github.com>
2023-11-07 00:10:39 +0900

    pf

--- works01/js/script.js
+++ works01/js/script.js
@@ -22,20 +22,41 @@
 	});
 
 
+
+	// カテゴリーの絞り込み
 	
-	$('.slider li').on('click', function(){
-		let cat = $(this).html();
-		$('.list').hide(); // .listを一度すべて非表示にする
-	  
-		// allの場合
-		if(cat == "all"){
-		  $('.list').hide("display","none");
-		  $('.list').fadeIn("display","block");
-		}
-		// 選択したカテゴリーだけを表示
-		$('.list').each(function(){
-		  if($(this).data('cat') == cat){
-			$(this).fadeIn();
-		  }
-		});
-	  });
+var searchItem = '.search_item';   // 絞り込む項目を選択するエリア
+var listItem = '.list';       // 絞り込み対象のアイテム
+var hideClass = 'is-hide';         // 絞り込み対象外の場合に付与されるclass名
+var activeClass = 'is-active';     // 選択中のグループに付与されるclass名
+
+$(function() {
+  // 絞り込みを変更した時
+  $(searchItem).on('click', function() {
+    $(searchItem).removeClass(activeClass);
+    var group = $(this).addClass(activeClass).data('group');
+    search_filter(group);
+  });
+});
+
+/**
+ * リストの絞り込みを行う
+ * @param {String} group data属性の値
+ */
+function search_filter(group) {
+  // 非表示状態を解除
+  $(listItem).removeClass(hideClass);
+  // 値が空の場合はすべて表示
+  if(group === '') {
+    return;
+  }
+  // リスト内の各アイテムをチェック
+  for (var i = 0; i < $(listItem).length; i++) {
+    // アイテムに設定している項目を取得
+    var itemData = $(listItem).eq(i).data('group');
+    // 絞り込み対象かどうかを調べる
+    if(itemData !== group) {
+      $(listItem).eq(i).addClass(hideClass);
+    }
+  }
+}
